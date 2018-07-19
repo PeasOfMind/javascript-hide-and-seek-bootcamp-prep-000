86d59b67617e82860e6ca84cdabf0c44e47b2c93
Yeqing Liu <sunsetpeak42@gmail.com>
2018-07-19 03:06:46 +0000

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,24 @@
+function getFirstSelector(selector){
+  return document.querySelector(selector);
+}
 
+function nestedTarget(){
+  return document.querySelector('#nested .target');
+}
+
+function increaseRankBy(n){
+  let rankedList = document.querySelectorAll('ul.ranked-list li');
+  for(let i = 0; i<rankedList.length; i++){
+    rankedList[i].innerHTML = parseInt(rankedList[i].innerHTML) + parseInt(n);
+  }
+}
+
+function deepestChild(){
+  let inner = document.querySelector('#grand-node');
+  let formerInner = "";
+  while(inner){
+    formerInner = inner;
+    inner = inner.querySelector('div');
+  }
+  return formerInner
+}
